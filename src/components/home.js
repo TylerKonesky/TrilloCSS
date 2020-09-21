6969b8dd0e8ef4a0a9759a60919e621bcda1a9c8
TylerKonesky <tylerkonesky@hotmail.com>
2020-09-21 16:21:14 -0600

    hotel overview

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import logo from '../images/logo.png';
 import icons from '../images/sprite.svg';
 import user from '../images/user.jpg';
+import hotel1 from '../images/hotel-1.jpg';
+import hotel2 from '../images/hotel-2.jpg';
+import hotel3 from '../images/hotel-3.jpg';
 
 class Home extends Component{
     render(){
@@ -64,7 +67,39 @@ class Home extends Component{
                         </div>
                     </nav>
                     <main className="hotel-view">
-                        Hotel View
+                        <div className="gallery">
+                            <figure className="gallery__item">
+                                <img src={hotel1} alt="photo of hotel 1" className="gallery__photo"></img>
+                            </figure>
+                            <figure className="gallery__item">
+                                <img src={hotel2} alt="photo of hotel 2" className="gallery__photo"></img>
+                            </figure>
+                            <figure className="gallery__item">
+                                <img src={hotel3} alt="photo of hotel 3" className="gallery__photo"></img>
+                            </figure>
+
+                        </div>
+
+                        <div className="overview">
+                            <h1 className="overview__heading">
+                                Hotel Las Palmas
+                            </h1>
+                            <div className="overview__star">
+                                <svg className="overview__icon-star"><use xlinkHref={`${icons}#icon-star`}></use></svg>
+                                <svg className="overview__icon-star"><use xlinkHref={`${icons}#icon-star`}></use></svg>
+                                <svg className="overview__icon-star"><use xlinkHref={`${icons}#icon-star`}></use></svg>
+                                <svg className="overview__icon-star"><use xlinkHref={`${icons}#icon-star`}></use></svg>
+                                <svg className="overview__icon-star"><use xlinkHref={`${icons}#icon-star`}></use></svg>
+                            </div>
+                            <div className="overview__location">
+                                <svg className="overview__icon-location"><use xlinkHref={`${icons}#icon-location-pin`}></use></svg>
+                                <button className="btn-inline">Albufeira, Portugal</button>
+                            </div>
+                            <div className="overview__rating">
+                                <div className="overview__rating-average">8.9</div>
+                                <div className="overview__rating-count">267 Votes</div>
+                            </div>
+                        </div>
                     </main>
                 </div>
             </div>
