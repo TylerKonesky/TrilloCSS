c54ca06c3371400f7d82c47150ec7c17ad056ffa
TylerKonesky <tylerkonesky@hotmail.com>
2020-09-21 20:17:40 -0600

    reviews

--- src/components/home.js
+++ src/components/home.js
@@ -5,6 +5,12 @@ import user from '../images/user.jpg';
 import hotel1 from '../images/hotel-1.jpg';
 import hotel2 from '../images/hotel-2.jpg';
 import hotel3 from '../images/hotel-3.jpg';
+import user1 from '../images/user-3.jpg';
+import user2 from '../images/user-4.jpg';
+import user3 from '../images/user-5.jpg';
+import user4 from '../images/user-6.jpg';
+import user5 from '../images/user-1.jpg';
+import user6 from '../images/user-2.jpg';
 
 class Home extends Component{
     render(){
@@ -98,6 +104,68 @@ class Home extends Component{
                             <div className="overview__rating">
                                 <div className="overview__rating-average">8.9</div>
                                 <div className="overview__rating-count">267 Votes</div>
+                            </div>
+                        </div>
+
+                        <div className="detail">
+                            <div className="description">
+                                <p className="paragraph">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi nisi dignissimos debitis ratione sapiente saepe. Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate.
+                                </p>
+                                <p className="paragraph">
+                                Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate delectus, inventore iure aliquid aliquam.
+                                </p>
+                                <ul className="list">
+                                    <li className="list__item">Close to the beach</li>
+                                    <li className="list__item">Breakfast Included</li>
+                                    <li className="list__item">Free airport shuttle</li>
+                                    <li className="list__item">Free Wifi in all rooms</li>
+                                    <li className="list__item">Air Conditioning and heating</li>
+                                    <li className="list__item">Pets Allowed</li>
+                                    <li className="list__item">We speak all languages</li>
+                                    <li className="list__item">Perfect for families</li>
+                                </ul>
+                                <div className="recommend">
+                                    <p className="recomment__count">
+                                        Lucy and 3 other friends recomment this hotel.
+                                    </p>
+                                    <div className="recommend__friends">
+                                        <img src={user1} alt="friend" className="recommend__photo"></img>
+                                        <img src={user2} alt="friend" className="recommend__photo"></img>
+                                        <img src={user3} alt="friend" className="recommend__photo"></img>
+                                        <img src={user4} alt="friend" className="recommend__photo"></img>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="user-review">
+                                <figure className="review">
+                                    <blockquote className="review__text">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta animi, totam, itaque officia ex.
+                                    </blockquote>
+                                    <figcaption className="review__user">
+                                        <img src={user5} alt="User Photo" className="review__photo"></img>
+                                        <div className="review__user-box">
+                                            <p className="review__user-name">Josh Tuckett</p>
+                                            <p className="review__user-date">Aug 19th, 2020</p>
+                                        </div>
+                                        <div className="review__rating">8.1</div>
+                                    </figcaption>
+                                </figure>
+                                <figure className="review">
+                                    <blockquote className="review__text">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta animi.
+                                    </blockquote>
+                                    <figcaption className="review__user">
+                                        <img src={user6} alt="User Photo" className="review__photo"></img>
+                                        <div className="review__user-box">
+                                            <p className="review__user-name">Samuel George</p>
+                                            <p className="review__user-date">Sep 3rd, 2020</p>
+                                        </div>
+                                        <div className="review__rating">8.1</div>
+                                    </figcaption>
+                                </figure>
+                                <button className="btn-inline">Show all <span>&rarr;</span></button>
                             </div>
                         </div>
                     </main>
